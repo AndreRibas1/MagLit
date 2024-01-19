ea5ea9132bdd4dba8e9ab96898a38074b5ba2965
André Ribas <andre@ribas.su>
2024-01-19 14:50:32 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,6 @@ import "../styles/globals.css";
 import Head from "next/head";
 import { BASE_URL } from "../utils/config";
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const CounterAnalytics = dynamic(
   () => import("../components/CounterAnalytics"),
@@ -51,7 +50,6 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Analytics />
-      <SpeedInsights />
       <CounterAnalytics />
       <RecoilRoot>
         <ThemeProvider attribute="class" defaultTheme="dark">
